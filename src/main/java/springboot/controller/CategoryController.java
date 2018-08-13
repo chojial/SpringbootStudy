7be7b75b22d90401f323c668124148eec304f775
Administrator <Administrator@192.168.0.104>
2018-08-13 19:43:08 +0800

    fenye

--- src/main/java/springboot/controller/CategoryController.java
+++ src/main/java/springboot/controller/CategoryController.java
@@ -2,6 +2,7 @@ package springboot.controller;
 
 import java.util.List;
 
+import org.hibernate.boot.archive.scan.spi.PackageInfoArchiveEntryHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,6 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 
 import springboot.Dao.CategoryDAO;
 import springboot.entity.Category;
@@ -25,35 +29,35 @@ public class CategoryController {
 	@Autowired
 	private CategoryDAO categoryDAO;
 	
-	@RequestMapping("/listCategory")
+	/*@RequestMapping("/listCategory")
     public String listCategory(Model m) throws Exception {
        List<Category> cs=categoryDAO.findAll();
           
         m.addAttribute("cs", cs);
           
         return "listCategory";
-    }
+    }*/
 	
-	@RequestMapping("/listCategory2")
+	/*@RequestMapping("/listCategory2")
     public String listCategory2(Model m) throws Exception {
        List<Category> cs=categoryMapper.findAll();
           
         m.addAttribute("cs", cs);
           
         return "listCategory";
-    }
+    }*/
 	
 	
-	@RequestMapping("/listCategory3")
+	/*@RequestMapping("/listCategory3")
     public String listCategory3(Model m) throws Exception {
        List<Category> cs=categoryMapper.findAll();
           
         m.addAttribute("cs", cs);
           
         return "listCategory";
-    }
+    }*/
 	
-	@RequestMapping("/addCategory")
+	/*@RequestMapping("/addCategory")
 	public String addCategory(Category c) throws Exception {
 		categoryDAO.save(c);
 		return "redirect:listCategory";
@@ -74,9 +78,9 @@ public class CategoryController {
 		Category c= categoryDAO.getOne(id);
 		m.addAttribute("c", c);
 		return "editCategory";
-	}
+	}*/
 	
-	@RequestMapping("/listCategory4")
+	/*@RequestMapping("/listCategory4")
 	public String listCategory4(Model m,@RequestParam(value = "start", defaultValue = "0") int start,@RequestParam(value = "size", defaultValue = "5") int size) throws Exception {
 		start = start<0?0:start;
 		Sort sort = new Sort(Sort.Direction.ASC, "id");
@@ -84,6 +88,40 @@ public class CategoryController {
 		Page<Category> page =categoryDAO.findAll(pageable);
 		m.addAttribute("page", page);
 	    return "listCategory4";
+	}*/
+	
+	@RequestMapping("/addCategory")
+	public String listCategory(Category c) throws Exception {
+		categoryMapper.save(c);
+		return "redirect:listCategory";
 	}
+	@RequestMapping("/deleteCategory")
+	public String deleteCategory(Category c) throws Exception {
+		categoryMapper.delete(c.getId());
+		return "redirect:listCategory";
+	}
+	@RequestMapping("/updateCategory")
+	public String updateCategory(Category c) throws Exception {
+		categoryMapper.update(c);
+		return "redirect:listCategory";
+	}
+	@RequestMapping("/editCategory")
+	public String listCategory(int id,Model m) throws Exception {
+		Category c= categoryMapper.get(id);
+		m.addAttribute("c", c);
+		return "editCategory";
+	}
+	
+	
+	@RequestMapping("/listCategory")
+	public String listCategory(Model m,@RequestParam(value="start",defaultValue="0")int start,
+			@RequestParam(value="size",defaultValue="5")int size) {
+		PageHelper.startPage(start, size,"id asc");
+		List<Category> cs=categoryMapper.findAll();
+		PageInfo<Category> page = new PageInfo<>(cs);
+		m.addAttribute("page", page);
+		return "listCategory2";
+	}
+
 	
 }
--- src/main/java/springboot/mapper/CategoryMapper.java
+++ src/main/java/springboot/mapper/CategoryMapper.java
@@ -2,12 +2,27 @@ package springboot.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import springboot.entity.Category;
 @Mapper
 public interface CategoryMapper {
-	//@Select("select * from category_")
+	@Select("select * from category_")
 	List<Category> findAll();
+	
+	@Select("select * from  category_ where id = #{id}")
+	public Category get(int id);
+	
+	@Insert("insert into category_ ( name ) values (#{name})")
+	public int save(Category category);
+	
+	@Delete("delete from category_ where id = #{id}")
+	public void delete(int id);
+	
+	@Update("update category_ set name = #{name} where id = #{id}")
+	public int update(Category category);
 }
